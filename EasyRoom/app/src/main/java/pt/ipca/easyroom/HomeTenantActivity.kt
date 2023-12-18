5142b9e219530842a84c2ac40239e1e75907c1c5
Rogerio Filho <a21868@alunos.ipca.pt>
2023-12-18 14:42:55 +0000

    User profile screen implemented.

--- EasyRoom/app/src/main/java/pt/ipca/easyroom/HomeTenantActivity.kt
+++ EasyRoom/app/src/main/java/pt/ipca/easyroom/HomeTenantActivity.kt
@@ -1,11 +1,25 @@
 package pt.ipca.easyroom
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class HomeTenantActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_tenant)
+
+        val ivLogOut = findViewById<ImageView>(R.id.ivLogOut)
+        ivLogOut.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        val ivProfile = findViewById<ImageView>(R.id.ivProfile)
+        ivProfile.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- EasyRoom/app/src/main/java/pt/ipca/easyroom/ProfileActivity.kt
+++ EasyRoom/app/src/main/java/pt/ipca/easyroom/ProfileActivity.kt
@@ -1,11 +1,62 @@
 package pt.ipca.easyroom
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
+
+        val tvFirstName = findViewById<TextView>(R.id.tvFirstName)
+        val tvLastName = findViewById<TextView>(R.id.tvLastName)
+        val tvEmailAddress = findViewById<TextView>(R.id.tvEmailAddress)
+        val tvPhoneNumber = findViewById<TextView>(R.id.tvPhoneNumber)
+        val tvUserType = findViewById<TextView>(R.id.tvUserType)
+
+        val user = auth.currentUser
+        if (user != null) {
+            db.collection("users").document(user.uid)
+                .get()
+                .addOnSuccessListener { document ->
+                    if (document != null) {
+                        tvFirstName.text = document.getString("firstName")
+                        tvLastName.text = document.getString("lastName")
+                        tvEmailAddress.text = document.getString("email")
+                        tvPhoneNumber.text = document.getString("phoneNumber")
+                        tvUserType.text = document.getString("userType")
+                    } else {
+                        Toast.makeText(
+                            baseContext,
+                            "Document not found.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
+                .addOnFailureListener { _ ->
+                    Toast.makeText(
+                        baseContext,
+                        "Error recovering data.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+        } else {
+            Toast.makeText(
+                baseContext,
+                "No current user",
+                Toast.LENGTH_SHORT,
+            ).show()
+        }
     }
 }
