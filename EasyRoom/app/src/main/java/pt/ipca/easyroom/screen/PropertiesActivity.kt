7ed4463f3fc2ddd2f46cd6b874044fa1c85a62e1
Rogerio Filho <a21868@alunos.ipca.pt>
2023-12-26 19:35:18 +0000

    Properties screen implemented.

--- EasyRoom/app/src/main/java/pt/ipca/easyroom/screen/PropertiesActivity.kt
+++ EasyRoom/app/src/main/java/pt/ipca/easyroom/screen/PropertiesActivity.kt
@@ -1,12 +1,51 @@
 package pt.ipca.easyroom.screen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import pt.ipca.easyroom.R
 
 class PropertiesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_properties)
+
+        val ivLogOut = findViewById<ImageView>(R.id.ivLogOut)
+        ivLogOut.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        val ivHome = findViewById<ImageView>(R.id.ivHome)
+        ivHome.setOnClickListener {
+            val intent = Intent(this, HomeOwnerActivity::class.java)
+            startActivity(intent)
+        }
+
+        val ivMyProperties = findViewById<ImageView>(R.id.ivMyProperties)
+        ivMyProperties.setOnClickListener {
+            val intent = Intent(this, MyPropertiesActivity::class.java)
+            startActivity(intent)
+        }
+
+        val ivAddProperty = findViewById<ImageView>(R.id.ivAddProperty)
+        ivAddProperty.setOnClickListener {
+            val intent = Intent(this, AddPropertyActivity::class.java)
+            startActivity(intent)
+        }
+
+        val ivDeleteProperty = findViewById<ImageView>(R.id.ivDeleteProperty)
+        ivDeleteProperty.setOnClickListener {
+            val intent = Intent(this, DeletePropertyActivity::class.java)
+            startActivity(intent)
+        }
+
+        val ivEditProperty = findViewById<ImageView>(R.id.ivEditProperty)
+        ivEditProperty.setOnClickListener {
+            val intent = Intent(this, EditPropertyActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
